568d4211beffe69422bed4e8e124188eb5f6e10d
utsha das <utshachandradas@gmail.com>
2024-02-03 21:33:14 +0600

    Topic for loop updated

--- Module-18/while.js
+++ Module-18/while.js
@@ -3,3 +3,18 @@ while (number < 5) {
   console.log("Hello BD");
   number = number + 1;
 }
+let numbers = 1;
+let sum = 0;
+while (numbers <= 10) {
+  sum += numbers;
+  numbers++;
+}
+console.log(sum);
+
+let even = 1;
+while (even <= 100) {
+  if (even % 2 == 0) {
+    console.log(even);
+  }
+  even++;
+}
