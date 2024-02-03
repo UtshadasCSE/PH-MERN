c3e0d741dad5d5dfd45e214f79e037ab5748f437
utsha das <utshachandradas@gmail.com>
2024-02-03 13:26:58 +0600

    Some function example updated

---
+++ Module-20/fun2.js
@@ -0,0 +1,9 @@
+// Age difference function
+
+function ageDiff(fatherAge, sonAge) {
+  const difference = fatherAge - sonAge;
+  console.log("Age difference is - ", difference);
+}
+const fatherAge = 40;
+const sonAge = 18;
+ageDiff(fatherAge, sonAge);
