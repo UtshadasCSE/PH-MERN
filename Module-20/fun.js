4da12dc5a1b2d420659cc11b8ab9ec4fe3f1d0c9
utsha das <utshachandradas@gmail.com>
2024-02-03 13:18:40 +0600

    Funcation topic updated

---
+++ Module-20/fun.js
@@ -0,0 +1,18 @@
+// function name () {}
+
+function sayHi() {
+  let Hi = "Hi";
+  console.log(Hi);
+}
+sayHi();
+
+function squre(number) {
+  console.log(number * number);
+}
+squre(5);
+
+function add(num1, num2) {
+  let sum = num1 + num2;
+  console.log(sum);
+}
+add(12, 12);
