eeb35c631f8510fbe68240c2ade12c1c5db29a4b
utsha das <utshachandradas@gmail.com>
2024-02-12 13:54:37 +0600

    Tea house updated

--- PROJECTS/Tea House/tailwind.config.js
+++ PROJECTS/Tea House/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 7px 50px 0px rgba(0, 0, 0, 0.06)",
+      },
+    },
   },
   plugins: [],
 };
