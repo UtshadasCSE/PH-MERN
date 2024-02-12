8b4555a33eb053f8f11242c10dd523758b470b46
utsha das <utshachandradas@gmail.com>
2024-02-12 21:51:02 +0600

    dom Updated

---
+++ Module-25/Js/bubble.js
@@ -0,0 +1,14 @@
+let items = document.getElementsByClassName("item");
+let addBtn = document.getElementById("addBtn");
+for (const item of items) {
+  item.addEventListener("click", function (event) {
+    event.target.parentNode.removeChild(event.target);
+  });
+}
+addBtn.addEventListener("click", function () {
+  let container = document.getElementById("ul-parent");
+  let li = document.createElement("li");
+  li.classList.add("item");
+  li.innerText = "IND Added";
+  container.appendChild(li);
+});
