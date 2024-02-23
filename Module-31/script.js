a7c168d2277cf2e2564670c305773ff0917fa101
utsha das <utshachandradas@gmail.com>
2024-02-23 11:56:43 +0600

    js chain in array and object

--- Module-31/script.js
+++ Module-31/script.js
@@ -1,6 +1,15 @@
 let city = "Dhaka";
 const person = {
   name: "Mj",
+  class: [
+    { it: "Delhi", iit: "karagpur" },
+    { bit: "Delhi", iit: "karagpur" },
+  ],
 };
 
 console.log(`Hi your name is ${person.name} and you live in ${city}`);
+
+const data = [{ name: "utsha", city: "cumilla", country: "BD" }];
+
+console.log(data[0].country);
+console.log(person.class[1].bit);
