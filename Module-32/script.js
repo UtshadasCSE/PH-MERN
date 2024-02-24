ce329adee2b0bc726bf1b15f8e8236b918664dd6
utsha das <utshachandradas@gmail.com>
2024-02-24 14:13:04 +0600

    falsy and truthy updated

---
+++ Module-32/script.js
@@ -0,0 +1,11 @@
+let numbers = [12, 12, 34, 565];
+let isBangladeshi = true;
+const student = { id: 20, class: 8 };
+
+console.log(typeof numbers);
+
+function add(a, b, c, d) {
+  let total = a + b + c + d;
+  console.log(a, b, c, d);
+}
+add(12, 12);
---
+++ Module-32/scriptt.js
@@ -0,0 +1,26 @@
+let a = 2;
+let b = "2";
+
+if (a === b) {
+  console.log("Equal");
+} else {
+  console.log("Not Equal");
+}
+
+let ab = 1;
+let ba = true;
+
+if (ab == ba) {
+  console.log("equal");
+} else {
+  console.log("Not equal");
+}
+
+let arr = [];
+let arrr = [];
+
+if (arr === arrr) {
+  console.log("Equal");
+} else {
+  console.log("Not equal");
+}
