24fe9e74f216db196e2b4ac85508cef7b05f26eb
utsha das <utshachandradas@gmail.com>
2024-03-13 00:20:23 +0600

    Data load react updated

--- PROJECTS/react-data-load/src/App.jsx
+++ PROJECTS/react-data-load/src/App.jsx
@@ -1,42 +1,18 @@
 import "./App.css";
 import Watch from "./components/Watch/Watch";
+import { useEffect, useState } from "react";
 function App() {
-  const watches = [
-    {
-      id: 1,
-      brand: "Apple",
-      model: "Apple Watch Series 7",
-      color: "Midnight",
-      price: 399,
-      features: ["Always-On Retina display", "ECG app", "Blood Oxygen app"],
-      image: "https://example.com/apple_watch_series_7.jpg",
-    },
-    {
-      id: 2,
-      brand: "Samsung",
-      model: "Galaxy Watch 4",
-      color: "Black",
-      price: 349,
-      features: ["BioActive Sensor", "Samsung Health app", "Built-in GPS"],
-      image: "https://example.com/galaxy_watch_4.jpg",
-    },
-    {
-      id: 3,
-      brand: "Fitbit",
-      model: "Fitbit Sense",
-      color: "Carbon/Graphite",
-      price: 299,
-      features: ["ECG app", "Built-in GPS", "Stress Management Score"],
-      image: "https://example.com/fitbit_sense.jpg",
-    },
-  ];
-
+  const [watches, setWatches] = useState([]);
+  useEffect(() => {
+    fetch("watches.json")
+      .then((res) => res.json())
+      .then((data) => setWatches(data));
+  }, []);
   return (
     <>
       <h1>Online Shop</h1>
       {watches.map((watch) => {
-        console.log()
-        <Watch watch={watch} />;
+        return <Watch key={watch.id} watch={watch} />;
       })}
     </>
   );
--- PROJECTS/react-data-load/src/components/Watch/Watch.jsx
+++ PROJECTS/react-data-load/src/components/Watch/Watch.jsx
@@ -1,12 +1,11 @@
-function Watch(watch) {
-  console.log(watch);
-  // const { brand, model } = watch;
-  // return (
-  //   <div>
-  //     <p>Model:{model}</p>
-  //     <p>Brand:{brand}</p>
-  //   </div>
-  // );
+function Watch({ watch }) {
+  const { model, brand } = watch;
+  return (
+    <div>
+      <p>Model:{model}</p>
+      <p>Brand:{brand}</p>
+    </div>
+  );
 }
 
 export default Watch;
