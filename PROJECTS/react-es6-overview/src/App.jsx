3a3f9cbc1978a857f0a61f04a792b375b45d525e
utsha das <utshachandradas@gmail.com>
2024-03-12 19:23:51 +0600

    ES6 overview Updated

---
+++ PROJECTS/react-es6-overview/src/App.jsx
@@ -0,0 +1,13 @@
+import Sunglass from "./Components/Sunglass/Sunglass";
+
+function App() {
+  return (
+    <>
+      <h1>Online Shop</h1>
+
+      <Sunglass />
+    </>
+  );
+}
+
+export default App;
---
+++ PROJECTS/react-es6-overview/src/components/Sunglass/Sunglass.jsx
@@ -0,0 +1,10 @@
+import Watch from "./../Watch/Watch";
+const Sunglass = () => {
+  return (
+    <div>
+      <h2>Hell</h2>
+    </div>
+  );
+};
+
+export default Sunglass;
