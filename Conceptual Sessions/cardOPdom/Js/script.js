8f3c03976b05a7c13aa8165e5afaab39986fd88a
utsha das <utshachandradas@gmail.com>
2024-03-14 23:06:13 +0600

    Dom master updated

--- Conceptual Sessions/cardOPdom/Js/script.js
+++ Conceptual Sessions/cardOPdom/Js/script.js
@@ -4,7 +4,9 @@ for (const btn of addToCart) {
   btn.addEventListener("click", function () {
     console.log("Ok");
     let cartNumber = document.getElementById("cartNumber");
+    let itemShow = document.getElementById("itemShow");
     count = count + 1;
     cartNumber.innerText = count;
+    itemShow.innerHTML = `${count} Items`;
   });
 }
