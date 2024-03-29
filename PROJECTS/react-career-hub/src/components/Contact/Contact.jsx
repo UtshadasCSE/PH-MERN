2d5bdaacf084868d0f68d92b5c773c84d851b47d
utsha das <utshachandradas@gmail.com>
2024-03-29 23:03:55 +0600

    Career Hub updated

--- PROJECTS/react-career-hub/src/components/Contact/Contact.jsx
+++ PROJECTS/react-career-hub/src/components/Contact/Contact.jsx
@@ -1,15 +1,6 @@
 import { Helmet } from "react-helmet-async";
-import React from "react";
-import GoogleMapReact from "google-map-react";
 
 const Contact = () => {
-  const defaultProps = {
-    center: {
-      lat: 10.99835602,
-      lng: 77.01502627,
-    },
-    zoom: 11,
-  };
   return (
     <div>
       <Helmet>
