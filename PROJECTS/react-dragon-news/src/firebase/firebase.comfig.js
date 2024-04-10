05d60c7af232a3d2b470243a48354cb59390c50e
utsha das <utshachandradas@gmail.com>
2024-04-10 11:31:07 +0600

    ENV file updated

--- PROJECTS/react-dragon-news/src/firebase/firebase.comfig.js
+++ PROJECTS/react-dragon-news/src/firebase/firebase.comfig.js
@@ -7,11 +7,11 @@ import { getAuth } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
-  authDomain: "the-dragon-news-26882.firebaseapp.com",
-  projectId: "the-dragon-news-26882",
-  storageBucket: "the-dragon-news-26882.appspot.com",
-  messagingSenderId: "612612308254",
-  appId: "1:612612308254:web:68584a9463659630979d0f",
+  authDomain: import.meta.env.VITE_AUTHDOMAIN,
+  projectId: import.meta.env.VITE_PROJECTID,
+  storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
